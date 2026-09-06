09eb70dc56fcb5656edc51d8760028ca39d39afc
Jade <jade@cachyos.org>
2026-09-06 18:29:29 +0100

    Fix app path per review: drop "./" prefix rejected by openFile:withApplication:andDeactivate:

--- testsuite/System/Library/Frameworks/AppKit.framework/test/NSWorkspace/manual/test_NSWorkspace_openFile_withApplication_andDeactivate.m
+++ testsuite/System/Library/Frameworks/AppKit.framework/test/NSWorkspace/manual/test_NSWorkspace_openFile_withApplication_andDeactivate.m
@@ -22,11 +22,13 @@ int main(void) {
 
         // A real application bundle can be launched with the file, so this
         // call is expected to succeed. Run this test from the directory that
-        // contains NSApplicationDelegate_application_openFile.app.
+        // contains NSApplicationDelegate_application_openFile.app. Note that
+        // openFile:withApplication:andDeactivate: rejects a "./" prefix on
+        // the path, so the bundle name is given without it.
         NSLog(@"Testing NSWorkspace openFile:withApplication:andDeactivate: manually...");
         NSLog(@"The launched app should log: Opened file: %@", dummyFile);
 
-        BOOL appResult = [workspace openFile:dummyFile withApplication:@"./NSApplicationDelegate_application_openFile.app" andDeactivate:NO];
+        BOOL appResult = [workspace openFile:dummyFile withApplication:@"NSApplicationDelegate_application_openFile.app" andDeactivate:NO];
         assert_is_true(appResult);
 
         // Clean up
